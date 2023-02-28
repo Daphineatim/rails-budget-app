d0e269309f38d0d42a9ddb0aa21c268a4dbdbb67
Daphineatim <hopedaphine19@gmail.com>
2023-02-28 13:40:54 -0700

    Add rspec to gem to gemfile

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  get "splashes#index"
+  get "splashes/index"
 
   resources :categories
   resources :expenses
+ 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
